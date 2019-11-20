183dad945a62d3a2ab2c441c570f3ac22800eeee
gabriele.lana <gabriele.lana@gmail.com>
2019-11-20 09:30:20 +0100

    Golden Master test

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,7 +1,22 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+describe("Golden Master", () => {
+  it("should not change it's behaviour", done => {
+    var fs = require("fs");
+    var exec = require("child_process").execFile;
+
+    fs.readFile(__dirname + "/GOLDEN_MASTER", "utf8", (err, content) => {
+      let executable = __dirname + "/../bin/run.js";
+      exec("node", [executable], (err, stdout, stderr) => {
+        expect(content).toBe(stdout.toString());
+        done();
+      });
+    });
+  });
+});
+
+describe("Gilded Rose", () => {
+  it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("fixme");
